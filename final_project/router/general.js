339d5950c4e1cc075eadc6bdb32f95123389d2a0
Sam Zabaldo <sam.zabaldo@gmail.com>
2024-05-31 14:21:49 -0400

    done task 12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,26 +32,56 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).json({books: JSON.stringify(books)});
+  let prom = new Promise((resolve, reject) => {
+    resolve(res.status(200).json({books: JSON.stringify(books)}));
+  })
+  prom.then((successMessage) => {
+    return successMessage;
+  })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   isbn = req.params.isbn
-  return res.status(200).json({books: books[isbn]});
+  
+  let prom = new Promise((resolve, reject) => {
+    resolve(res.status(200).json({books: books[isbn]}))
+  })
+  prom.then((successMessage) => {
+    return successMessage;
+  })
  });
   
+// // Get book details based on author
+// public_users.get('/author/:author',function (req, res) {
+//   author = req.params.author; 
+//   let ret_books = []
+//   const keys = Object.keys(books);
+//   for (let key in keys) {
+//     if (books[parseInt(key)+1]['author'] == author) {
+//       ret_books.push(books[parseInt(key)+1])
+//     }
+//   }
+//   return res.status(200).json({books: ret_books});
+// });
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   author = req.params.author; 
   let ret_books = []
-  const keys = Object.keys(books);
-  for (let key in keys) {
-    if (books[parseInt(key)+1]['author'] == author) {
-      ret_books.push(books[parseInt(key)+1])
+  let prom = new Promise((resolve, reject) => {
+    const keys = Object.keys(books);
+    for (let key in keys) {
+      if (books[parseInt(key)+1]['author'] == author) {
+        ret_books.push(books[parseInt(key)+1])
+      }
     }
-  }
-  return res.status(200).json({books: ret_books});
+    resolve(res.status(200).json({books: ret_books}))
+  })
+  prom
+    .then((successMessage) => {
+      return successMessage;
+    })
 });
 
 // Get all books based on title
