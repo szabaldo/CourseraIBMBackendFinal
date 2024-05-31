a7ba614d31aeb31612ffedcb99d45a9cce4e5208
Sam Zabaldo <sam.zabaldo@gmail.com>
2024-05-31 14:24:38 -0400

    finished lab

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -51,19 +51,6 @@ public_users.get('/isbn/:isbn',function (req, res) {
     return successMessage;
   })
  });
-  
-// // Get book details based on author
-// public_users.get('/author/:author',function (req, res) {
-//   author = req.params.author; 
-//   let ret_books = []
-//   const keys = Object.keys(books);
-//   for (let key in keys) {
-//     if (books[parseInt(key)+1]['author'] == author) {
-//       ret_books.push(books[parseInt(key)+1])
-//     }
-//   }
-//   return res.status(200).json({books: ret_books});
-// });
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -88,13 +75,18 @@ public_users.get('/author/:author',function (req, res) {
 public_users.get('/title/:title',function (req, res) {
   title = req.params.title; 
   let ret_books = []
-  const keys = Object.keys(books);
-  for (let key in keys) {
-    if (books[parseInt(key)+1]['title'] == title) {
-      ret_books.push(books[parseInt(key)+1])
+  let prom = new Promise((resolve, reject) => {
+    const keys = Object.keys(books);
+    for (let key in keys) {
+      if (books[parseInt(key)+1]['title'] == title) {
+        ret_books.push(books[parseInt(key)+1])
+      }
     }
-  }
-  return res.status(200).json({books: ret_books});
+    resolve(res.status(200).json({books: ret_books}))
+  })
+  prom.then((successMessage) => {
+    return successMessage;
+  })
 });
 
 //  Get book review
