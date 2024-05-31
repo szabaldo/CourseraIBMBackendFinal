1cce5dc10b1f99a8fd8e0570111df02029e65ff0
Sam Zabaldo <sam.zabaldo@gmail.com>
2024-05-30 20:36:41 -0400

    finished task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,14 +23,28 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  author = req.params.author; 
+  let ret_books = []
+  const keys = Object.keys(books);
+  for (let key in keys) {
+    if (books[parseInt(key)+1]['author'] == author) {
+      ret_books.push(books[parseInt(key)+1])
+    }
+  }
+  return res.status(200).json({books: ret_books});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  title = req.params.title; 
+  let ret_books = []
+  const keys = Object.keys(books);
+  for (let key in keys) {
+    if (books[parseInt(key)+1]['title'] == title) {
+      ret_books.push(books[parseInt(key)+1])
+    }
+  }
+  return res.status(200).json({books: ret_books});
 });
 
 //  Get book review
